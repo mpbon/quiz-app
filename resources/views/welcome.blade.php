5a6ba7133003670c209507768af8b0b50eb48116
mpbon <mattpaley@hotmail.com>
2019-11-19 19:39:32 -0700

    moved styles to app.scss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,74 +16,24 @@
 
         <!-- Styles -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-                margin-bottom: 0px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-                margin: 10px;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-
-            #app {
-                padding-bottom: 30px;
-            }
 
         </style>
+        
     </head>
     <body>
         <div class="flex-center position-ref full-height">
             <div class="content">
+
+                <div class="logo">
+                    <img src="images/quiz-app-logo.png" alt="multiple choice quiz logo">
+                </div>
+
                 <div class="title m-b-md">
                     Web-Dev Quiz App
                 </div>
 
                 @if (Route::has('login'))
-                    <div class="links">
+                    <div class="links wlinks">
                         @auth
                             <a href="{{ url('/home') }}">Home</a>
                         @else
